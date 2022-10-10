eff4a4fbeed7fd7c9b6edd6eefe8607a2c39a26f
JosePerez <perezruiz902@gmail.com>
2022-10-10 00:18:51 -0500

    GUI sin imágenes

--- Creator.py
+++ Creator.py
@@ -1,20 +1,16 @@
 ########################################################################
 ### CREADOR DE CARPETA, PLANILLA Y UBICACIÓN ESPACIAL DE CLIENTE GTD ###
 ########################################################################
-# Código para cambiar de .ui a .py en consola
-# pyuic5 -x Principal_Window2.ui -o interfaz.py
 
 # Importamos las librerias que necesitaremos (istaladas previamente)
 from PyQt5 import QtWidgets, uic
 import os, shutil, simplekml
 
-# os.chdir(r'c:/Users/Jose Luis/Desktop/FOLDER_FACTIBILIDADES')
-
 # Iniciar la aplicación
 app = QtWidgets.QApplication([])
 
 # Cargar archivos .ui
-principal = uic.loadUi("Principal_Window2.ui")
+principal = uic.loadUi("principal_window.ui")
 creado = uic.loadUi("creado.ui")
 error = uic.loadUi("error.ui")
 
@@ -28,27 +24,24 @@ def gui_principal():
     if len(codigo)==0 or len(cliente)==0 or len(distrito)==0 or len(lat)==0 or len(lon)==0:
         principal.alerta.setText("Ingrese la información completa")
     elif type(lat)==str or type(lon)!=str or type(cliente)!=str:
-        gui_crear()
+        # Definimos el nombre de nuestros archivos
+        client_name = f'{codigo} - Factibilidad {cliente} - {distrito}'
 
-def gui_crear():
-    # Definimos el nombre de nuestros archivos
-    client_name = f'{codigo} - Factibilidad {cliente} - {distrito}'
+        # Creamos la carpeta del cliente
+        os.mkdir(client_name)
 
-    # Creamos la carpeta del cliente
-    os.mkdir(client_name)
+        # Creamos una copia de la planilla dentro de la carpeta del cliente
+        shutil.copy('PLANILLA.xlsm', f'{client_name}/{client_name}.xlsm')
 
-    # Creamos una copia de la planilla dentro de la carpeta del cliente
-    shutil.copy('PLANILLA.xlsm', f'{client_name}/{client_name}.xlsm')
+        # Creamos el kml de ubicación del cliente y la guardamos en la carpeta
+        kml = simplekml.Kml()
+        kml.newpoint(name=cliente, coords=[(lon, lat)])  # lon, lat, optional height
+        kml.save(f'{client_name}/{client_name}.kmz')
 
-    # Creamos el kml de ubicación del cliente y la guardamos en la carpeta
-    kml = simplekml.Kml()
-    kml.newpoint(name=cliente, coords=[(lon, lat)])  # lon, lat, optional height
-    kml.save(f'{client_name}/{client_name}.kmz')
-
-    # Abrimos los archivos creados
-    os.chdir(client_name)
-    os.startfile(f'{client_name}.xlsm')
-    os.startfile(f'{client_name}.kmz')
+        # Abrimos los archivos creados
+        os.chdir(client_name)
+        os.startfile(f'{client_name}.xlsm')
+        os.startfile(f'{client_name}.kmz')
 
     principal.hide()
     creado.show()
